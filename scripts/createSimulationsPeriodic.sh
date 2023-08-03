17501079343bd437e340538af28dc5c70cf76358
Frans Liqui Lung <frans.liquilung@gmail.com>
2023-08-03 15:21:41 +1000

    adjusted create and run scripts

--- scripts/createSimulationsPeriodic.sh
+++ scripts/createSimulationsPeriodic.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 # Create folders for simulations and copy initial profiles
+pathDALES="../build/src/"
 pathInput="../input/"
 pathCases="../cases/"
 pathPeriodic="${pathCases}periodic/"
@@ -8,3 +9,5 @@ mkdir $pathPeriodic
 cp "${pathInput}profiles/prof.inp.xxx" "${pathPeriodic}${experiment}/prof.inp.000"
 cp "${pathInput}profiles/lscale.inp.xxx" "${pathPeriodic}${experiment}/lscale.inp.000"
 cp "${pathInput}namoptions/namoptions.periodic" "${pathPeriodic}${experiment}/namoptions"
+ln -s "${pathDALES}dales4.4" "${pathPeriodic}${experiment}/."
+ln -s merge.sh "${pathPeriodic}${experiment}/." 

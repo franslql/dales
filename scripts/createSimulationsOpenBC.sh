04a1fadf7073e799ef9c2482cbb493c613140d7f
Frans Liqui Lung <frans.liquilung@gmail.com>
2023-08-03 15:59:35 +1000

    Fixed creation files and renamed run files

--- scripts/createSimulationsOpenBC.sh
+++ scripts/createSimulationsOpenBC.sh
@@ -2,22 +2,27 @@
 # Create folders for simulations and copy initial profiles
 sigmax_array=( 000 002 004 008 016 )
 sigmat_array=( 000 006 030 180 )
-pathDALES="../build/src/"
-pathInput="../input/"
 pathCases="../cases/"
 pathOpenBC="${pathCases}openBC/"
 mkdir -p $pathCases
 mkdir -p $pathOpenBC
+cd $pathOpenBC
 for sigmat in ${sigmat_array[@]}; do
 	for sigmax in ${sigmax_array[@]}; do
 		experiment="x${sigmax}y${sigmax}z000t${sigmat}"
 		# create simulation directory
 		mkdir -p "${pathOpenBC}${experiment}"
-		cp "${pathInput}initial_profiles/prof.inp.xxx" "${pathOpenBC}${experiment}/prof.inp.001"
-		cp "${pathInput}initial_profiles/lscale.inp.xxx" "${pathOpenBC}${experiment}/lscale.inp.001"
-		cp "${pathInput}namoptions/namoptions.openBC" "${pathOpenBC}${experiment}/namoptions"
-		ln -s "${pathInput}boundary_input/openboundaries.inp.${experiment}.nc" "${pathOpenBC}${experiment}/openboundaries.inp.001.nc"
-		ln -s "${pathDALES}dales4.4" "${pathOpenBC}${experiment}/."
-		ln -s merge.sh "${pathOpenBC}${experiment}/." 
+		cd $experiment
+		rm -f dales4.4
+		rm -f merge.sh
+		rm -f openboundaries.inp.001.nc
+		cp ../../../input/initial_profiles/prof.inp.xxx prof.inp.001
+		cp ../../../input/initial_profiles/lscale.inp.xxx lscale.inp.001
+		cp ../../../input/namoptions/namoptions.openBC namoptions
+		ln -s "../../../input/boundary_input/openboundaries.inp.${experiment}.nc" openboundaries.inp.001.nc
+		ln -s ../../../build/src/dales4.4 .
+		ln -s ../../../scripts/merge.sh .
+		cd ..
 	done
 done
+cd ../../scripts/
--- scripts/runOpenBC.sh
+++ scripts/runOpenBC.sh
@@ -1,17 +1,12 @@
 #!/bin/bash
 sigmax_array=( 000 002 004 008 016 )
 sigmat_array=( 000 006 030 180 )
-pathScripts=$(pwd)
 pathCases="../cases/"
-pathOpenBC_synturb="${pathCases}openBC_synturb/"
-cd $pathOpenBC_synturb
+pathOpenBC="${pathCases}openBC/"
+cd $pathOpenBC
 for sigmat in ${sigmat_array[@]}; do
 	for sigmax in ${sigmax_array[@]}; do
 		experiment="x${sigmax}y${sigmax}z000t${sigmat}"
-    # Skip simulation without smoothing as it's the same as the openBC simulation
-    if [ $experiment = "x000y000z000t000" ]; then
-      continue
-    fi
     cd $experiment
     mpirun -np 8 ./dales4.4 2>&1 | tee output_sim.txt
     ./merge.sh 2>&1 | tee output_merge.txt 
@@ -26,4 +21,4 @@ for sigmat in ${sigmat_array[@]}; do
     cd ..
   done
 done
-cd $pathScripts
+cd ../../scripts/

c46e734f6f7245864c80622adab3ce0cdd30eb5f
franslql <84774133+franslql@users.noreply.github.com>
2023-10-27 15:38:03 +1100

    Update runOpenBC.sh to run all required simulations

--- scripts/runOpenBC.sh
+++ scripts/runOpenBC.sh
@@ -1,8 +1,6 @@
 #!/bin/bash
-# sigmax_array=( 000 002 004 008 016 )
-# sigmat_array=( 000 006 030 180 )
-sigmax_array=( 000 )
-sigmat_array=( 000 )
+sigmax_array=( 000 002 004 008 016 )
+sigmat_array=( 000 006 030 180 )
 pathCases="../cases/"
 pathOpenBC="${pathCases}openBC/"
 cd $pathOpenBC
